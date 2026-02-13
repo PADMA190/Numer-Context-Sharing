7b83fd28b2d2adb5e8c8562345b04e03f4e96559
padmalatchi <padmalatchi@gmail.com>
2026-02-13 18:31:27 +0530

    Convert backend for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -8,8 +8,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 app.use("/api/numer", numerRoutes);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
